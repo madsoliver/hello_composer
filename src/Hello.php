15748866f2f8a9b867e7f6e42aed6555774fd730
Mads Oliver Jenssen <mads@komplettnettbutikk.no>
2021-01-12 14:16:43 +0100

    init

---
+++ src/Hello.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+ * (c) Mads Oliver Jenssen 
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Komplettnettbutikk\HelloComposer;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
